f7c11a58250327e5abe474734054da50977eea59
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2022-04-05 11:56:58 +0900

    Create svfg for UseDefAnalysis

--- llvm/lib/Analysis/SVF/UseDefAnalysis/UseDefSVFGBuilder.cpp
+++ llvm/lib/Analysis/SVF/UseDefAnalysis/UseDefSVFGBuilder.cpp
@@ -1,5 +1,5 @@
 #include "UseDefAnalysis/UseDefSVFGBuilder.h"
-//#include "Graphs/SVFG.h"
+#include "MemoryModel/PointerAnalysisImpl.h"  // BVDataPTAImpl
 
 using namespace SVF;
 using namespace SVFUtil;
@@ -15,18 +15,79 @@ void UseDefSVFGBuilder::buildSVFG() {
 }
 
 void UseDefSVFGBuilder::rmDerefDirSVFGEdges(BVDataPTAImpl *pta) {
-  // TODO
+  for (const auto it : *svfg) {
+    const SVFGNode *node = it.second;
+    if (const StmtSVFGNode *stmtNode = SVFUtil::dyn_cast<StmtSVFGNode>(node)) {
+      if (SVFUtil::isa<StoreSVFGNode>(stmtNode)) {
+        const SVFGNode *def = svfg->getDefSVFGNode(stmtNode->getPAGDstNode());
+        if (SVFGEdge *edge = svfg->getIntraVFGEdge(def, stmtNode, SVFGEdge::IntraDirectVF))
+          svfg->removeSVFGEdge(edge);
+      } else if (SVFUtil::isa<LoadSVFGNode>(stmtNode)) {
+        const SVFGNode *def = svfg->getDefSVFGNode(stmtNode->getPAGSrcNode());
+        if (SVFGEdge *edge = svfg->getIntraVFGEdge(def, stmtNode, SVFGEdge::IntraDirectVF))
+          svfg->removeSVFGEdge(edge);
+      }
+    }
+  }
 }
 
 bool UseDefSVFGBuilder::isStrongUpdate(const SVFGNode *node, NodeID &singleton, BVDataPTAImpl *pta) {
   bool isSU = false;
+  if (const StoreSVFGNode *store = SVFUtil::dyn_cast<StoreSVFGNode>(node)) {
+    const PointsTo &dstCPSet = pta->getPts(store->getPAGDstNodeID());
+    if (dstCPSet.count() == 1) {
+      // Find the unique element in cpts
+      PointsTo::iterator it = dstCPSet.begin();
+      singleton = *it;
+
+      // Strong update can be made if this points-to target is not heap, array or field-insensitive.
+      if (!pta->isHeapMemObj(singleton) && !pta->isArrayMemObj(singleton)
+          && SVFIR::getPAG()->getBaseObj(singleton)->isFieldInsensitive() == false
+          && !pta->isLocalVarInRecursiveFun(singleton)) {
+        isSU = true;
+      }
+    }
+  }
   return isSU;
 }
 
 void UseDefSVFGBuilder::rmIncomingEdgeForSUStore(BVDataPTAImpl *pta) {
-  // TODO
+  SVFGEdgeSet toRemove;
+  for (const auto it : *svfg) {
+    const SVFGNode *node = it.second;
+    if (const StmtSVFGNode *stmtNode = SVFUtil::dyn_cast<StmtSVFGNode>(node)) {
+      if (SVFUtil::isa<StoreSVFGNode>(stmtNode) && SVFUtil::isa<StoreInst>(stmtNode->getValue())) {
+        NodeID singleton;
+        if (isStrongUpdate(node, singleton, pta)) {
+          for (const auto inEdge : node->getInEdges()) {
+            if (inEdge->isIndirectVFGEdge()) {
+              toRemove.insert(inEdge);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for (SVFGEdge *edge : toRemove) {
+    svfg->removeSVFGEdge(edge);
+  }
 }
 
 void UseDefSVFGBuilder::rmDirOutgoingEdgeForLoad(BVDataPTAImpl *pta) {
-  // TODO
+  SVFGEdgeSet toRemove;
+  for (const auto it : *svfg) {
+    const SVFGNode *node = it.second;
+    if (const StmtSVFGNode *stmtNode = SVFUtil::dyn_cast<StmtSVFGNode>(node)) {
+      if (SVFUtil::isa<LoadSVFGNode>(stmtNode) && SVFUtil::isa<LoadInst>(stmtNode->getValue())) {
+        for (const auto outEdge : stmtNode->getOutEdges()) {
+          toRemove.insert(outEdge);
+        }
+      }
+    }
+  }
+
+  for (SVFGEdge *edge : toRemove) {
+    svfg->removeSVFGEdge(edge);
+  }
 }
